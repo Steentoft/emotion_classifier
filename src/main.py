d010912a69c69a927104a0a92e314e52b1450eb3
Steentoft <chrsteentoft@gmail.com>
2026-05-04 15:42:58 +0200

    tokenize

--- src/main.py
+++ src/main.py
@@ -1,10 +1,34 @@
-
+import numpy
 from datasets import load_dataset
+import tiktoken
+
+enc = tiktoken.get_encoding("o200k_base")
 
 ds = load_dataset("dair-ai/emotion", "split")
 
-test = ds["train"][0]
+test = ds["train"]
 
-print(test)
+def tokenize_split(split):
+    result = []
+    for text in split:
+        result.append({ "text" : enc.encode(text["text"]), "label" : text["label"] })
+    return result
 
-print(test["text"].split())
+list = tokenize_split(test)
+
+tokens = []
+for i in range(len(list)):
+    tokens.append(len(list[i]["text"]))
+
+print(tokens)
+
+print(numpy.median(tokens))
+
+
+dict = {}
+for text in test:
+    if len(text["text"]) in dict:
+        dict.append({ len(text["text"]) : 1 })
+    else:
+        dict[len(text["text"])] = dict[len(text["text"])] + 1
+print(dict)
