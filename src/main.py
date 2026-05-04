714ab98b614ae3b670db0b2e5026412a00c6ee13
Steentoft <chrsteentoft@gmail.com>
2026-05-04 15:12:42 +0200

    test case

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
 
 from datasets import load_dataset
-ds = load_dataset("dair-ai/emoOon", "split")
+
+ds = load_dataset("dair-ai/emotion", "split")
+
+test = ds["train"][0]
+
+print(test)
+
+print(test["text"].split())
